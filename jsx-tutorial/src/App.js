a6856f62c33e079bbac583f9871e5b0dc78e31fa
Daniel Ashby <danielmashby@gmail.com>
2023-04-10 14:13:29 +0100

    The second tutorial from Digital Ocean

---
+++ jsx-tutorial/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './App.css';
+
+const displayEmojiName = event => alert(event.target.id);
+const emojis = [
+  {
+    emoji: "😀",
+    name: "grinning face"
+  },
+  {
+    emoji: "🎉",
+    name: "party popper"
+  },
+  {
+    emoji: "💃",
+    name: "woman dancing"
+  }
+];
+
+
+function App() {
+  const greeting = "greeting";
+  const displayAction = false;
+
+  return(
+    <div className="container">
+      <h1 id={greeting}>Hello, World</h1>
+      {displayAction && <p>I am writing JSX</p>} {/* Only display the paragraph if displayAction is true */}
+      <ul>
+        {/*
+          <li>
+              <button onClick={displayEmojiName}>
+                <span role="img" aria-label="grinning face" id="grinning face">😀</span>
+              </button>
+          </li>
+          <li>
+            <button onClick={displayEmojiName}>
+                <span role="img" aria-label="party popper" id="party popper">🎉</span>
+            </button>
+          </li>
+          <li>
+            <button onClick={displayEmojiName}>
+                <span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
+            </button>
+          </li>
+        */}
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name}>
+              <button onClick={displayEmojiName}>
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))
+        }        
+      </ul>
+    </div>
+  )
+}
+
+export default App;
